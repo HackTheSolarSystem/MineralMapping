0d3760602c98cdcc8593dd62b01062eadac9e227
Underwood <john.underwood@signafire.com>
2019-02-09 23:25:07 -0500

    add basic summary and detail view

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DetailViewComponent } from './views/detail-view/detail-view.component';
+import { SummaryViewComponent } from './views/summary-view/summary-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailViewComponent,
+    SummaryViewComponent,
   ],
   imports: [
     BrowserModule
