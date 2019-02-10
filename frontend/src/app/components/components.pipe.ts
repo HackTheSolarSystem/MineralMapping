2465459db89f43d01eb1771dc8a596df84a9d6a0
Underwood <john.underwood@signafire.com>
2019-02-10 10:49:42 -0500

    update colors

--- frontend/src/app/components/components.pipe.ts
+++ frontend/src/app/components/components.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class GetColorPipe implements PipeTransform {
   private colorMap = {
-    "Troilite": 'red',
-    "Taenite": 'orange',
-    "Pyroxene": 'yellow',
-    "Pentlandite": 'green',
-    "Olivine": 'blue',
-    "Millerite": 'indigo',
-    "Kamacite": 'violet',
+    "Troilite": '#4D79A8',
+    "Taenite": '#F28E2C',
+    "Pyroxene": '#E05758',
+    "Pentlandite": '#76B7B2',
+    "Olivine": '#58A14E',
+    "Millerite": '#EDC949',
+    "Kamacite": '#B07AA1',
   };
   transform(number) {
     return this.colorMap[number];
