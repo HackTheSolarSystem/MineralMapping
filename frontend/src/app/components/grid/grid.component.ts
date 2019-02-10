85a4f39bb5f8c8eb67cfe3d7fd1c0c07b11df84c
Underwood <john.underwood@signafire.com>
2019-02-10 10:38:43 -0500

    add real data to grid and render it

--- frontend/src/app/components/grid/grid.component.ts
+++ frontend/src/app/components/grid/grid.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
+import data from '../../constants/obj2_array.json';
+
 @Component({
   selector: 'overlay-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss'],
 })
 export class GridComponent {
+  constructor() {
+    console.log('data in grid is:', data);
+  }
+  public realData = data ? data.data : [];
   public testArray = [
     [1, 2, 3, 4, 6, 6, 7, 8, 9, 5, 5],
     [1, 2, 3, 4, 6, 6, 7, 8, 9, 5, 5],
