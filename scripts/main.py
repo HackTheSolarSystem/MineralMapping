e85cb3857317e06088d9638e34e1444a88694ce7
Jeremy Neiman <docmarionum1@gmail.com>
2019-02-12 00:19:54 -0500

    load standard intensities into df

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,10 @@
 import argparse
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
+from skimage.io import imread
+
 parser = argparse.ArgumentParser(description='Predict the mineral content of a meteorite given spectrometer imagary.')
 parser.add_argument('standards', type=str, help='a directory containing the standards.')
 parser.add_argument('--bits', default='32', help='image bit-depth to use (8 or 32)')
@@ -8,10 +12,39 @@ parser.add_argument('--bits', default='32', help='image bit-depth to use (8 or 3
 args = parser.parse_args()
 print(args)
 
-standard_path = Path(args.standards)
+def load_standards_df(args):
+    standard_path = Path(args.standards)
 
-standard_masks = [
-    i for i in list(standard_path.glob('*_mask.tif')) if 'obj' not in i.name
-]
+    # Get the mask paths
+    standard_masks = [
+        i for i in list(standard_path.glob('*_mask.tif')) if 'obj' not in i.name
+    ]
 
-print(standard_masks)
+    # Load the individual element images for the standards
+    standard_elements = [
+        {'name': s.name.split('_')[2].split('.')[0], 'image': imread(s)}
+        for s in standard_path.glob('standards_%s*.tif' % args.bits)
+    ]
+
+    dfs = []
+    for s in standard_masks:
+        mask = imread(s)
+        pixels = []
+        for element in standard_elements:
+            # Get the pixels in the mask location for each element
+            pixels.append(element['image'][mask > 0])
+
+        # Create a dataframe of the pixels
+        df = pd.DataFrame(
+            np.dstack(pixels)[0], columns=[i['name'] for i in standard_elements]
+        )
+        df['mineral'] = s.stem.split('_')[0]
+        dfs.append(df)
+
+    # Concatenate all of the dataframes
+    df = pd.concat(dfs).reset_index(drop=True)
+
+    return df
+
+df = load_standards_df(args)
+print(df.head())
