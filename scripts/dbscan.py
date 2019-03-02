3ec2a2274c974f362af7f39c536f81cf3b95bec7
Peter Kang <peterjkang@gmail.com>
2019-03-01 23:32:21 -0500

    Adjust unknown minimum calculation

--- scripts/dbscan.py
+++ scripts/dbscan.py
@@ -37,7 +37,7 @@ def get_predicted_weights(obj_df, standards_characteristics, calculate_unknown=T
         # add column for unknown weight percent
         percent_weight_pred['unknown'] = np.ones(len(percent_weight_pred)) - \
                 percent_weight_pred.sum(axis=1)
-        percent_weight_pred['unknown'][percent_weight_pred['unknown'] < 0] = 0
+        percent_weight_pred['unknown'] = np.minimum(percent_weight_pred['unknown'], np.zeros(percent_weight_pred['unknown'].shape))
 
     return percent_weight_pred
 
